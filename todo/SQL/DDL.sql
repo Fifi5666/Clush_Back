39b7b1e57be1abecd648c1e84df29f85cb76764a
Fifi5666 <halo_xx00@naver.com>
2024-09-10 17:33:09 +0900

    수정

--- todo/SQL/DDL.sql
+++ todo/SQL/DDL.sql
@@ -16,3 +16,13 @@ CREATE TABLE `todo` (
 -- 기본 CRUD 이외의 우선순위 설정 기능 추가
 ALTER TABLE todo
 ADD COLUMN priority ENUM('LOW', 'MEDIUM', 'HIGH') DEFAULT 'MEDIUM';
+
+INSERT INTO `todo` (`name`, `status`, `due_date`, `priority`) VALUES
+('할일 1', 0, '2024-09-15', 'HIGH'),
+('할일 2', 0, '2024-09-20', 'MEDIUM'),
+('할일 3', 1, '2024-09-25', 'LOW'),
+('할일 4', 0, '2024-09-30', 'MEDIUM'),
+('할일 5', 1, '2024-10-05', 'HIGH');
+
+TRUNCATE todo;
+SELECT * FROM todo;
